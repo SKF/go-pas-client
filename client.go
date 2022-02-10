7af4ef9a174019f99f49ee9d4abcf924edd7caf3
Johan Svan <johan.svan@skf.com>
2022-02-10 15:14:42 +0100

    Add get threshold method
    Adds the client function to fetch a threshold on a node.

--- client.go
+++ client.go
@@ -1,13 +1,18 @@
 package client
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/SKF/go-pas-client/models"
 	rest "github.com/SKF/go-rest-utility/client"
 	"github.com/SKF/go-utility/v2/stages"
+	"github.com/SKF/go-utility/v2/uuid"
 )
 
-type API interface{}
+type API interface {
+	GetThreshold(context.Context, uuid.UUID) (models.Threshold, error)
+}
 
 type Client struct {
 	*rest.Client
@@ -17,10 +22,10 @@ var _ API = &Client{Client: nil}
 
 func WithStage(stage string) rest.Option {
 	if stage == stages.StageProd {
-		return rest.WithBaseURL("https://api.point-alarm-status.sandbox.iot.enlight.skf.com/v1")
+		return rest.WithBaseURL("https://api.point-alarm-status.iot.enlight.skf.com")
 	}
 
-	return rest.WithBaseURL(fmt.Sprintf("https://api.point-alarm-status.%s.iot.enlight.skf.com/v1", stage))
+	return rest.WithBaseURL(fmt.Sprintf("https://api.point-alarm-status.%s.iot.enlight.skf.com", stage))
 }
 
 func New(opts ...rest.Option) *Client {
@@ -32,4 +37,18 @@ func New(opts ...rest.Option) *Client {
 	)
 
 	return &Client{restClient}
+}
+
+func (c *Client) GetThreshold(ctx context.Context, nodeID uuid.UUID) (models.Threshold, error) {
+	request := rest.Get("v1/point-alarm-threshold/{nodeId}").
+		Assign("nodeId", nodeID).
+		SetHeader("Accept", "application/json")
+
+	var threshold models.Threshold
+
+	if err := c.DoAndUnmarshal(ctx, request, &threshold); err != nil {
+		return models.Threshold{}, fmt.Errorf("getting threshold failed: %w", err)
+	}
+
+	return threshold, nil
 }
