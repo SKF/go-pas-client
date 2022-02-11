6db5b17c91a965543bfcc86c71c16942a3436c2a
Johan Svan <johan.svan@skf.com>
2022-02-11 11:49:26 +0100

    Add function to set threshold
    Adds a client function that can be used to an alarm threshold on a node.

--- client.go
+++ client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	internal_models "github.com/SKF/go-pas-client/internal/models"
 	"github.com/SKF/go-pas-client/models"
 	rest "github.com/SKF/go-rest-utility/client"
 	"github.com/SKF/go-utility/v2/stages"
@@ -12,6 +13,7 @@ import (
 
 type API interface {
 	GetThreshold(context.Context, uuid.UUID) (models.Threshold, error)
+	SetThreshold(context.Context, uuid.UUID, models.Threshold) error
 }
 
 type Client struct {
@@ -44,11 +46,30 @@ func (c *Client) GetThreshold(ctx context.Context, nodeID uuid.UUID) (models.Thr
 		Assign("nodeId", nodeID).
 		SetHeader("Accept", "application/json")
 
-	var threshold models.Threshold
+	var response internal_models.ModelsGetPointAlarmThresholdResponse
 
-	if err := c.DoAndUnmarshal(ctx, request, &threshold); err != nil {
+	if err := c.DoAndUnmarshal(ctx, request, &response); err != nil {
 		return models.Threshold{}, fmt.Errorf("getting threshold failed: %w", err)
 	}
 
+	threshold := models.Threshold{} // nolint:exhaustivestruct
+
+	if err := threshold.FromInternal(response); err != nil {
+		return models.Threshold{}, fmt.Errorf("converting threshold failed: %w", err)
+	}
+
 	return threshold, nil
+}
+
+func (c *Client) SetThreshold(ctx context.Context, nodeID uuid.UUID, threshold models.Threshold) error {
+	request := rest.Put("v1/point-alarm-threshold/{nodeId}").
+		Assign("nodeId", nodeID).
+		WithJSONPayload(threshold.ToInternal()).
+		SetHeader("Accept", "application/json")
+
+	if _, err := c.Do(ctx, request); err != nil {
+		return fmt.Errorf("request failed: %w", err)
+	}
+
+	return nil
 }
